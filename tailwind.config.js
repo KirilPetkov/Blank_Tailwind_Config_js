c6598a327f1f1b04bcd5f7a381b0e3e9956dcdd8
Kiril Petkov <49308732+KirilPetkov@users.noreply.github.com>
2022-11-13 22:56:38 +0000

    Add files via upload
    This is Blank tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */ 
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    // Some useful comment
+    fontFamily: {
+      'PS2P': ['"Press Start 2P"'] // Ensure fonts with spaces have " " surrounding it.
+      // Some useful comment
+    },
+  },
+  plugins: [
+    // Some useful comment
+  ],
+}
